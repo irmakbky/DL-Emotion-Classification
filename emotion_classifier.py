eadb050338bfc97b49f27c41ee74e62baca805d6
Irmak <apple@apple-MacBook-Air-3.local>
2020-11-16 12:37:35 -0800

    added dropouts

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -2,8 +2,10 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
-DATASET_PATH = "data.json"
+DATASET_PATH = "re_smaller_json.json"
 
 def load_data(dataset_path):
 
@@ -15,6 +17,28 @@ def load_data(dataset_path):
     targets = np.array(data["labels"])
 
     return inputs, targets
+
+def plot_history(history):
+
+    fig, axs = plt.subplots(2)
+
+    # accuracy subplot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # error subplot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
 
 if __name__ == "__main__":
 
@@ -34,15 +58,16 @@ if __name__ == "__main__":
 
         # 1st hidden layer
         keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.3),
+
 
         # 2nd hidden layer
         keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.3),
 
         # 3rd hidden layer
         keras.layers.Dense(64, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.BatchNormalization(),
+        keras.layers.Dropout(0.3),
 
         # output layer
         keras.layers.Dense(10, activation="softmax")
@@ -50,7 +75,7 @@ if __name__ == "__main__":
     ])
 
     # compile network
-    optimizer = keras.optimizers.Nadam(learning_rate=0.0001)
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
     model.compile(optimizer=optimizer,
                   loss="sparse_categorical_crossentropy",
                   metrics=["accuracy"])
@@ -58,5 +83,8 @@ if __name__ == "__main__":
     model.summary()
 
     # train network
-    model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test),
+    history = model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test),
               epochs=100, batch_size=32)
+
+    # plot accuracy - error over epochs
+    plot_history(history)
