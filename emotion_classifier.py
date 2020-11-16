a2156ce9e06ad8f531a2c53b51443e6672ca5be7
Irmak <apple@apple-MacBook-Air-3.local>
2020-11-16 09:15:14 -0800

    classifier works, issues with overfitting

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,5 +1,9 @@
 import json
 import numpy as np
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
+DATASET_PATH = "data.json"
 
 def load_data(dataset_path):
 
@@ -15,11 +19,41 @@ def load_data(dataset_path):
 if __name__ == "__main__":
 
     # load data
+    inputs, targets = load_data(DATASET_PATH)
 
     # get train and test sets
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs,
+                                                                              targets,
+                                                                              test_size=0.3)
 
     # build network
+    model = keras.Sequential([
+
+        # input layer
+        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+
+        # 1st hidden layer
+        keras.layers.Dense(512, activation="relu"),
+
+        # 2nd hidden layer
+        keras.layers.Dense(256, activation="relu"),
+
+        # 3rd hidden layer
+        keras.layers.Dense(64, activation="relu"),
+
+        # output layer
+        keras.layers.Dense(10, activation="softmax")
+
+    ])
 
     # compile network
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(optimizer=optimizer,
+                  loss = "sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+
+    model.summary()
 
     # train network
+    model.fit(inputs_train, targets_train, validation_data=(inputs_test, targets_test),
+              epochs=100, batch_size=32)
