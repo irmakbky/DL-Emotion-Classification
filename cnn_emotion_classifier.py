a063c4d1027f38fbcea3bc10e16a95e29203f817
Irmak <apple@apple-MacBook-Air-3.local>
2020-11-17 02:49:49 -0800

    final

--- cnn_emotion_classifier.py
+++ cnn_emotion_classifier.py
@@ -18,19 +18,49 @@ def load_data(data_path):
 def train_val_test_split(test_size, validation_size):
 
     # load data
+    X, y = load_data(DATA_PATH)
+
     # create train/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
     # create train/validation split
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train,
+                                                                    test_size=validation_size)
+
+    # we need a 4d array
+    X_train = X_train[..., np.newaxis]
+    X_validation = X_validation[..., np.newaxis]
+    X_test = X_test[..., np.newaxis]
 
     return X_train, X_validation, X_test, y_train, y_validation, y_test
 
 def build_model(input_shape):
 
     # create model
+    model = keras.Sequential()
 
     # 1st conv layer
+    model.add(keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding="same"))
+    model.add(keras.layers.BatchNormalization())
+
     # 2nd conv layer
+    model.add(keras.layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding="same"))
+    model.add(keras.layers.BatchNormalization())
+
     # 3rd conv layer
+    model.add(keras.layers.Conv2D(32, (2, 2), activation="relu", input_shape=input_shape))
+    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding="same"))
+    model.add(keras.layers.BatchNormalization())
+
+    # flatten output and feed it into dense layer
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation="relu"))
+    model.add(keras.layers.Dropout(0.3))
+
     # output layer
+    model.add(keras.layers.Dense(10, activation="softmax"))
 
     return model
 
@@ -40,7 +70,7 @@ def predict(model, X, y):
 
     prediction = model.predict(X)
     predicted_index = np.argmax(prediction, axis=1)
-    
+
     print("Expected index: {}, Predicted index: {}".format(y, predicted_index))
 
 if __name__ == "__main__":
